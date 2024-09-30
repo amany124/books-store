628d1bd055459dd082386d077bd96978baf0f158
amany <amany@gmail.com>
2024-10-01 00:22:04 +0300

    make animation in splash view

--- lib/Features/splash_feature/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash_feature/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,47 @@
 import 'package:bookly/core/assetsData.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController slide_controller;
+  late Animation<Offset> slide_animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the animation controller
+    slide_controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+          milliseconds: 700), // Reduced duration for smoother transition
+    );
+
+    // Define the slide animation from offset (0, 2) to offset (0, 0)
+    slide_animation = Tween<Offset>(
+      begin: const Offset(0, 5), // Start position: below the screen
+      end: Offset.zero, // End position: original position
+    ).animate(
+      slide_controller,
+    );
+
+    // Start the animation
+    slide_controller.forward();
+  }
+
+  @override
+  void dispose() {
+    // Dispose the animation controller to free resources
+    slide_controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +49,24 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
-        const SizedBox(
-          height: 15,
+        // Animate the logo sliding into view
+        SlideTransition(
+          position: slide_animation,
+          child: Image.asset(AssetsData.logo),
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontFamily: 'Poppins',
+        const SizedBox(
+          height: 20,
+        ),
+        // Animate the text sliding into view
+        SlideTransition(
+          position: slide_animation,
+          child: const Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 18,
+            ),
           ),
         ),
       ],
